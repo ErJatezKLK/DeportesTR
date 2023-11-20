efbe9ae959092dc4d71c97d7fe764c2d1bf7f950
Javier Mateos <118256079+ErJatezKLK@users.noreply.github.com>
2023-11-20 10:21:51 +0100

    actualizado pa introducir usuario

--- app/src/main/java/com/example/deportestr/ui/models/User.kt
+++ app/src/main/java/com/example/deportestr/ui/models/User.kt
@@ -4,10 +4,10 @@ package com.example.deportestr.ui.models
 
 
 data class User(
-    var id : Int,
+    var id : Int?,
     var name : String,
     var email : String,
     var password : String,
-    var team : List<Team>,
-    var players: List<Athlete>
+    var team : List<Team>?,
+    var players: List<Athlete>?
 )
