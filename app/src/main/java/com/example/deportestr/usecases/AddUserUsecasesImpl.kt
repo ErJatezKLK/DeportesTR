0a4f0b2d16db7ab4778ec8b7f715175750bc45c0
Javier Mateos <118256079+ErJatezKLK@users.noreply.github.com>
2023-11-20 10:18:05 +0100

    Update AddUserUsecasesImpl.kt

--- app/src/main/java/com/example/deportestr/usecases/AddUserUsecasesImpl.kt
+++ app/src/main/java/com/example/deportestr/usecases/AddUserUsecasesImpl.kt
@@ -7,8 +7,8 @@ import javax.inject.Inject
 class AddUserUsecasesImpl @Inject constructor(
     private val deportesRepository: DeportesRepository
 ): AddUserUseCases {
-    override suspend fun addUser(): Response<Void> {
-        return deportesRepository.addUser()
+    override suspend fun addUser(user: User): Response<Void> {
+        return deportesRepository.addUser(user)
     }
 
 }
