645a037c31513ec0358c3448908dfaca60d0630e
javier <mateossanchezjavier@gmail.com>
2023-11-21 00:10:52 +0100

    borrar

--- AddUserUseCases.kt
+++
@@ -1,8 +0,0 @@
-package com.example.deportestr.usecases
-
-import com.example.deportestr.ui.models.User
-import retrofit2.Response
-
-interface AddUserUseCases {
-    suspend fun addUser(user: User): Response<Void>
-}
